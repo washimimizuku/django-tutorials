9dd1328fb1d1957595afba0007feb3f6fe055760
Nuno Barreto <nbarreto@gmail.com>
2020-04-17 13:37:49 +0200

    Add user authentication

--- users/forms.py
+++ users/forms.py
@@ -8,11 +8,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields
+        fields = ('username', 'email', 'age',)
