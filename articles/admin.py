b9d247dea42259e159b4462ec2b4d1d650617899
Nuno Barreto <nbarreto@gmail.com>
2020-04-18 10:57:28 +0200

    Add model Comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
