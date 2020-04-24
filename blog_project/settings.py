804f6d1ed16dc28fdd4ea9b8eea433ebaef086da
Nuno Barreto <nbarreto@gmail.com>
2020-04-24 22:28:23 +0200

    Add API documentation with CoreAPI

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -140,7 +140,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
