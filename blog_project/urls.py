22f684fe43b954acdc1e41959cf513232cf815a5
Nuno Barreto <nbarreto@gmail.com>
2020-04-24 22:47:04 +0200

    Add API Schema and Documentation with drf-yasg

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,12 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 API_TITLE = 'Blog API'
 API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
-schema_view = get_schema_view(title=API_TITLE)
+schema_view = get_schema_view(
+   openapi.Info(
+      title=API_TITLE,
+      default_version='v1',
+      description=API_DESCRIPTION,
+   ),
+   #public=True,
+   #permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +42,7 @@ urlpatterns = [
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
     path('schema/', schema_view),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
