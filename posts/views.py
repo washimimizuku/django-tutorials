a78fdce59765281a60225c7e3c5f6449437f55ef
Nuno Barreto <nbarreto@gmail.com>
2020-04-24 22:10:41 +0200

    Implement ViewSets

--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,17 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions
+from rest_framework import viewsets
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly, )
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
