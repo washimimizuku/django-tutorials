67be6fae433186d7cc16c9195c99d9067ff17a8c
Nuno Barreto <nbarreto@gmail.com>
2020-04-25 15:39:51 +0200

    Improve hyperlinking

--- code_highlighter_project/settings.py
+++ code_highlighter_project/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
