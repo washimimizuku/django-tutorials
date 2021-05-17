98f91b6052effb7b9db7e29bc878a41ebcdcd8ce
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-17 21:52:49 +0200

    Add create superuser

--- udemy-recipe-api/app/core/tests/test_models.py
+++ udemy-recipe-api/app/core/tests/test_models.py
@@ -36,3 +36,13 @@ class ModelTests(TestCase):
                 email=None,
                 password='test123'
             )
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='test123@email.com',
+            password='test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
