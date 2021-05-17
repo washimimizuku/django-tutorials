7ddc24ddc48f84b60110f91a740b11fa236dcb42
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-17 21:42:36 +0200

    Normalize email

--- udemy-recipe-api/app/core/tests/test_models.py
+++ udemy-recipe-api/app/core/tests/test_models.py
@@ -17,3 +17,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@EXAMPLE.COM'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
