ab2ac5b248e3d7c88f071687f68e8b6678b25cb6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-18 12:13:47 +0200

    Create tests por user api

--- udemy-recipe-api/app/user/tests/test_user_api.py
+++ udemy-recipe-api/app/user/tests/test_user_api.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from rest_framework.tests import APIClient
+from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
