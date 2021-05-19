b873319d4aa6253dbb99aeb4092b1b49bc90fd4d
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-19 10:38:45 +0200

    Add image field to recipe model

--- udemy-recipe-api/app/app/settings.py
+++ udemy-recipe-api/app/app/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = '/volume/web/static'
+MEDIA_ROOT = '/volume/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
